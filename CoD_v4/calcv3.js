e5c61b6652e92c884565a62561d689e1f0d4d600
Ildsjel <jo-hoenscheid@t-online.de>
2018-09-09 00:12:29 +0200

    Fixed styling bugs of the calculator inputs

--- CoD_v4/calcv3.js
+++ CoD_v4/calcv3.js
@@ -3,26 +3,26 @@
 function valueTypeInput() {
 
 	if (document.getElementById('redcos').checked) {
-		document.getElementById('redcosinput').style.visibility = 'visible';
+		document.getElementById('redcosinput').style.display = 'block';
 	}
-	else document.getElementById('redcosinput').style.visibility = 'hidden';
+	else document.getElementById('redcosinput').style.display = 'none';
 
 
 	if (document.getElementById('avocos').checked) {
-		document.getElementById('avocosinput').style.visibility = 'visible';
+		document.getElementById('avocosinput').style.display = 'block';
 	}
-	else document.getElementById('avocosinput').style.visibility = 'hidden';
+	else document.getElementById('avocosinput').style.display = 'none';
 
 
 	if (document.getElementById('increv').checked) {
-		document.getElementById('increvinput').style.visibility = 'visible';
+		document.getElementById('increvinput').style.display = 'block';
 	}
-	else document.getElementById('increvinput').style.visibility = 'hidden';
+	else document.getElementById('increvinput').style.display = 'none';
 
 	if (document.getElementById('prorev').checked) {
-		document.getElementById('prorevinput').style.visibility = 'visible';
+		document.getElementById('prorevinput').style.display = 'block';
 	}
-	else document.getElementById('prorevinput').style.visibility = 'hidden';
+	else document.getElementById('prorevinput').style.display = 'none';
 
 }
 
