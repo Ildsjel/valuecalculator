cb54368544ba3057c49e997bb551fdb13725f6ce
Ildsjel <jo-hoenscheid@t-online.de>
2018-09-13 23:35:08 +0200

    - added probability calculation via combinatorics and laplace

--- CoD_v4/probcalc.js
+++ CoD_v4/probcalc.js
@@ -1,14 +1,23 @@
 /*probability calculation*/
 function calculateprobability () {
-  var riskpercentageperyear = document.getElementById("avocosrisk"); /*INPUT FIELD IN HTML*/
+  var riskpercentageperyear = document.getElementById("avocosrisk").value; /*INPUT FIELD IN HTML*/
+  console.log(riskpercentageperyear);
   var weeksperyear = 52; /*The total number of objects is weeks per year which results always in 4 weeks per month*/
+  console.log(weeksperyear);
   var affectedweeksofyear = (riskpercentageperyear * weeksperyear) / 100;
-  var possiblecombinations = math.combinations(weeksperyear, affectedweeksofyear); /*!!THIS IS DOESNT WORK BECAUSE IT EXPECTS INTS!!*/
-  var probability = possiblecombinations;
+  console.log(affectedweeksofyear);
+  var roundedaffectedweeksofyear = (affectedweeksofyear).toFixed(0); /*float needs to be cut for math function. otherwise it throws an execption*/
+  console.log(roundedaffectedweeksofyear);
+  var possiblecombinations = math.combinations(weeksperyear, roundedaffectedweeksofyear); /*calculates all combinations possible*/
+  console.log(possiblecombinations);
+  var probability = (affectedweeksofyear / possiblecombinations) * 100; /*Laplace probability calc*/
+  console.log(probability);
+  var roundedprobability = (probability).toFixed(2); /*rounding of percentage to make it readable*/
+  console.log(roundedprobability);
 
     if (probability !=0){
 
-      document.getElementById("output2").innerHTML=probability;
+      document.getElementById("output2").innerHTML=roundedprobability;
     }
     else {
       console.log("error")
