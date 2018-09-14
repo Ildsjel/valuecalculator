a6f6f359d4c69b727878f0b301fd73571682743a
Ildsjel <jo-hoenscheid@t-online.de>
2018-09-15 01:07:19 +0200

    first api design running

--- CoD_v4/app.js
+++ CoD_v4/app.js
@@ -1,13 +1,31 @@
 // app.js
 
 const express = require("express");
+const bodyParser = require("body-parser");
+const taskController = require("./controller/TaskController");
+
 
 // db instance connection
 require("./config/db");
 
 const app = express();
 
-const port = process.env.PORT || 3301;
+const port = process.env.PORT || 3303;
+app.use(bodyParser.urlencoded({ extended: true })); /*bodyParser is not defined: INEED TO FIX THAT*/
+app.use(bodyParser.json());
+
+// API ENDPOINTS
+
+app
+  .route("/tasks")
+  .get(taskController.listAllTasks)
+  .post(taskController.createNewTask);
+
+app
+  .route("/tasks/:taskid")
+  .get(taskController.readTask)
+  .put(taskController.updateTask)
+  .delete(taskController.deleteTask);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
