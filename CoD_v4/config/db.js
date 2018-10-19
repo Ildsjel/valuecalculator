13dff69cf4b457d413c8a9217bc4c13ba7871ac6
Ildsjel <jo-hoenscheid@t-online.de>
2018-10-19 16:41:51 +0200

    db.js: corrected creds

--- CoD_v4/config/db.js
+++ CoD_v4/config/db.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 
 const dbURI =
-  "mongodb://johannes:n3cr0m4ni4c666!@valuecalc-shard-00-00-0nvz7.mongodb.net:27017,valuecalc-shard-00-01-0nvz7.mongodb.net:27017,valuecalc-shard-00-02-0nvz7.mongodb.net:27017/test?ssl=true&replicaSet=ValueCalc-shard-0&authSource=admin&retryWrites=true"
+  "mongodb://johannes:N3cr0m4ni4c666!@valuecalc-shard-00-00-0nvz7.mongodb.net:27017,valuecalc-shard-00-01-0nvz7.mongodb.net:27017,valuecalc-shard-00-02-0nvz7.mongodb.net:27017/test?ssl=true&replicaSet=ValueCalc-shard-0&authSource=admin&retryWrites=true"
 const options = {
   reconnectTries: Number.MAX_VALUE,
   poolSize: 10
